f9c2b6d5e720af7f1cc90c20094f7ec774151be0
Thiago Henrique Prochnow <thiprochnow@gmail.com>
2021-12-20 17:25:43 -0300

    Implement Strategy Design pattern

--- src/br/com/alura/loja/TestesImpostos.java
+++ src/br/com/alura/loja/TestesImpostos.java
@@ -3,7 +3,7 @@ package br.com.alura.loja;
 import java.math.BigDecimal;
 
 import br.com.alura.loja.imposto.CalculadoraDeImpostos;
-import br.com.alura.loja.imposto.TipoImposto;
+import br.com.alura.loja.imposto.ICMS;
 import br.com.alura.loja.orcamento.Orcamento;
 
 public class TestesImpostos {
@@ -11,6 +11,6 @@ public class TestesImpostos {
     public static void main(String[] args) {
         Orcamento orcamento = new Orcamento(new BigDecimal("100"));
         CalculadoraDeImpostos calculador = new CalculadoraDeImpostos();
-        System.out.println(calculador.calcular(orcamento, TipoImposto.ICMS));
+        System.out.println(calculador.calcular(orcamento, new ICMS()));
     }
 }
